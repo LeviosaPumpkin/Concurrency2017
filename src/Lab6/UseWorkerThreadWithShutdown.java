f658cccf168fa596d91ca1d7c9aaae51721a5ea6
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2017-07-17 11:40:39 +0300

    HW got InterruptedException after 2 workers

---
+++ src/Lab6/UseWorkerThreadWithShutdown.java
@@ -0,0 +1,31 @@
+package Lab6;
+
+import Lab1.Utils;
+
+public class UseWorkerThreadWithShutdown {
+	
+	public static void main(String[] args) {
+		WorkerThreadWithShutdown worker = new WorkerThreadWithShutdown();
+		worker.submit(() -> {
+			System.out.println("start" + Thread.currentThread());
+			Utils.pause(1000);
+			System.out.println("end");
+		});
+		
+		worker.submit(() -> {
+			System.out.println("start" + Thread.currentThread());
+			Utils.pause(1000);
+			System.out.println("end");
+		});
+		Utils.pause(10000);
+		worker.shutdown();
+		worker.submit(() -> {
+			System.out.println("start" + Thread.currentThread());
+			Utils.pause(1000);
+			System.out.println("end");
+		});
+		
+		
+	}
+
+}
